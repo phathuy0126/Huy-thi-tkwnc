22c3fc3284c4faf6920bbb9feefcd74063049a83
phathuy0126 <phathuy0126@gmail.com>
2022-03-03 21:15:31 +0700

    up code

---
+++ js/global.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    setTimeout(() => {
+        $('.windowLoad').remove();
+    }, 100);
+})
--- js/localStorage.js
+++ js/localStorage.js
@@ -1,3 +1,4 @@
+'use strict';
 function createStorage(key) {
     const store = JSON.parse(localStorage.getItem(key)) || {};
     const save = () => {
@@ -20,25 +21,16 @@ function createStorage(key) {
 }
 
 
-function formatNumber(a, b, c, d) {
-    var e = isNaN(b = Math.abs(b)) ? 2 : b;
-    b = void 0 == c ? "," : c;
-    d = void 0 == d ? "," : d;
-    c = 0 > a ? "-" : "";
-    var g = parseInt(a = Math.abs(+a || 0).toFixed(e)) + "",
-        n = 3 < (n = g.length) ? n % 3 : 0;
-    return c + (n ? g.substr(0, n) + d : "") + g.substr(n).replace(/(\d{3})(?=\d)/g, "$1" + d) + (e ? b + Math.abs(a - g).toFixed(e).slice(2) : "")
-}
-
-
 const product = createStorage('product');
 const productAddCart = createStorage('productAddCart');
 
 
 const itemProducts = document.querySelectorAll('.item-product');
 const handleGetProduct = (e) => {  
-    const parentEle = e.target.parentElement.parentElement
-    const img = parentEle.querySelector('a img').getAttribute('src');
+    const parentEle = e.target.classList.contains('priceNew') || e.target.classList.contains('priceOld') ? 
+    e.target.parentElement.parentElement.parentElement :
+    e.target.parentElement.parentElement    
+    const img = parentEle.querySelector('img').getAttribute('src');
     const name = parentEle.querySelector('.item-product-if p:first-child').textContent;
     const priceNew = parentEle.querySelector('.item-product-if .priceNew').textContent;
     const priceOld = parentEle.querySelector('.item-product-if .priceOld').textContent;    
@@ -47,8 +39,9 @@ const handleGetProduct = (e) => {
         name: name,
         priceNew: priceNew,
         priceOld: priceOld
-    }
-    product.set('infoProduct', infoProduct);
+    }    
+    product.set('infoProduct', infoProduct);    
+
     console.log(product.get('infoProduct'));
 }
 itemProducts.forEach(itemProduct => {
@@ -56,8 +49,3 @@ itemProducts.forEach(itemProduct => {
 })
 
 
-window.addEventListener('load', function () {
-    setTimeout(() => {
-        $('.windowLoad').remove();
-    }, 100);
-})
